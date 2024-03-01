d4a3915d046ab410e1ca753a1893c3df3a1c8d2e
Natalie Lim <natalielim1906@gmail.com>
2024-02-29 20:31:36 -0800

    tested

--- Etherium.java
+++ Etherium.java
@@ -2,14 +2,16 @@
 
 public class Etherium {
     public static void main (String [] args) {
-
+        double[] range = {2.0, 1.0, 133.0, 2.0, 53.0, 5.0};
+        System.out.println (sumOfSquares(3, 3));
+        System.out.println (range(range));
     }
 
-    public double sumOfSquares (double first, double second) {
+    public static double sumOfSquares (double first, double second) {
         return ((first * first) + (second * second));
     }
 
-    public double range (double[] numbers) {
+    public static double range (double[] numbers) {
         double max = numbers[0];
         double min = numbers[0];
         for (int i = 1; i < numbers.length; i++) {
